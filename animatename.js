ce9498145299c71c51611dfc21e41844fcd35b85
spideynn <spideynn@gmail.com>
2014-04-08 19:00:06 -0400

    Testing a JS creation

---
+++ animatename.js
@@ -0,0 +1,16 @@
+var red = [0, 100, 40];
+var orange = [20, 100, 50];
+var green = [75, 100, 40];
+var blue = [222, 80, 50];
+var purple = [300, 60, 60];
+
+var myName = "Spideynn";
+var letterColors = [red, orange, green, blue, purple];
+if(15>5) {bubbleShape = "circle";
+}
+else {bubbleShape = "square";
+}
+
+
+drawName(myName, letterColors);
+bounceBubbles()
